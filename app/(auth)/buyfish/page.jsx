d8a607f9603abf46633cdb6f668991ea3b36aacf
Shakil hossain <developershakil2@gmail.com>
2023-09-21 17:35:29 +0600

    Update page.jsx
    Updated minimum buy

--- app/(auth)/buyfish/page.jsx
+++ app/(auth)/buyfish/page.jsx
@@ -37,9 +37,9 @@ const BuyFish =()=>{
         setModalHandle('flex');
         setModalTitle("please enter minumum amount to buy product");
       }else{
-        if (amount && amount < 1000) {
+        if (amount && amount < 100000) {
           setModalHandle('flex')
-          setModalTitle("minimum buy 1000 peso ");
+          setModalTitle("minimum buy 100000 peso ");
         }else{
           axios
           .post('https://fishfarm.onrender.com/buyfish',
